ffbdf14977bcbdee9811c9edcf960466e950445d
Claire Peng <pwrclaire@gmail.com>
2024-06-15 01:12:00 -0700

    Done

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,59 +1,55 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import banner from "../../src/components/banner";
+import style from "./style.module.css";
 
-export default function Home() {
+export default function ContactUs() {
+  const form = useRef();
+  const [isSent, setIsSent] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_bdffdem",
+        "template_9zl7qsw",
+        form.current,
+        "user_KIP4sbmL9itetpkyz3R88"
+      )
+      .then(
+        (result) => {
+          setIsSent(true);
+          console.log(result);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <form className="container">
-      <h1>Get in touch</h1>
-      <div className="email block">
-        <label htmlFor="frm-email">Email</label>
-        <input
-          id="frm-email"
-          type="email"
-          name="email"
-          autoComplete="email"
-          required
-        />
-      </div>
-      <div className="block phone">
-        <label htmlFor="frm-phone">Phone</label>
-        <input
-          id="frm-phone"
-          type="text"
-          name="phone"
-          autoComplete="tel"
-          required
-        />
-      </div>
-      <div className="name block">
-        <div>
-          <label htmlFor="frm-first">First Name</label>
-          <input
-            id="frm-first"
-            type="text"
-            name="first"
-            autoComplete="given-name"
-            required
-          />
+    <div>
+      {banner()}
+      {isSent ? (
+        <div className={style.thankYouMessage}>
+          <h2>Thank You!</h2>
+          <p>
+            Your message has been sent successfully. We will get back to you
+            shortly.
+          </p>
         </div>
-        <div>
-          <label htmlFor="frm-last">Last Name</label>
-          <input
-            id="frm-last"
-            type="text"
-            name="last"
-            autoComplete="family-name"
-            required
-          />
-        </div>
-      </div>
-      <div className="message block">
-        <label htmlFor="frm-message">Message</label>
-        <textarea id="frm-message" rows="6" name="message"></textarea>
-      </div>
-      <div className="button block">
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+      ) : (
+        <form ref={form} onSubmit={sendEmail} className={style.form}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      )}
+    </div>
   );
 }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -4,18 +4,18 @@ const Navbar = () => {
   const Buttons = () => {
     return (
       <div className={styles.navbar}>
-        <a href="/" className={styles["a-button"]}>
+        {/* <a href="/" className={styles["a-button"]}>
           <span>HOME</span>
-        </a>
+        </a> */}
         <a href="/portraits" className={styles["a-button"]}>
           <span>PORTRAIT</span>
         </a>
         <a href="/automotive" className={styles["a-button"]}>
           <span>AUTOMOTIVE</span>
         </a>
-        {/* <a href="/contact" className={styles["a-button"]}>
+        <a href="/contact" className={styles["a-button"]}>
           CONTACT
-        </a> */}
+        </a>
       </div>
     );
   };
